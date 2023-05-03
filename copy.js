9107645f38520eeab7894e56b82b452b2db89333
Duncan <duncantheinvictus@gmail.com>
2023-05-03 18:05:10 +0800

    merge js files

--- copy.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("copy-text-btn").onclick = function () {
-  navigator.clipboard
-    .writeText(document.getElementById("copy-text").innerText)
-    .then(function () {
-      alert("Email has been copied!");
-    });
-};
--- index.js
+++ index.js
@@ -135,3 +135,12 @@ function animate() {
 }
 
 animate();
+
+// clipboard
+document.getElementById("copy-text-btn").onclick = function () {
+  navigator.clipboard
+    .writeText(document.getElementById("copy-text").innerText)
+    .then(function () {
+      alert("Email has been copied!");
+    });
+};
