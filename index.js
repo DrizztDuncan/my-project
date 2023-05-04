c8edbe6a987e6b056b113e8601396ee9c3884b75
DrizztDuncan <duncantheinvictus@gmail.com>
2023-05-04 16:42:30 +0800

    camera location

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ const renderer = new THREE.WebGLRenderer({
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth * 1.2, window.innerHeight * 1.2);
+renderer.setSize(window.innerWidth * 2, window.innerHeight * 2);
 camera.position.setZ(30);
-camera.position.setX(-3);
+camera.position.setX(-10);
 
 renderer.render(scene, camera);
 
